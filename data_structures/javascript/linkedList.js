b99762b1e0e7c5463cb2b3c198db52833fece7d7
Peter Caulfield <peterjcaulfield@gmail.com>
2016-01-14 21:09:41 +0000

    adding impl of the insert

--- data_structures/javascript/linkedList.js
+++ data_structures/javascript/linkedList.js
@@ -8,7 +8,12 @@ function linkedList(array) {
   this.length = 0;
   this.first = this.last = null;
 
-  //TODO: loop input and insert
+  array.each(function(value) {
+
+    this.push(new node(value)); 
+
+  }.bind(this));
+
 }
 
 linkedList.prototype = {
@@ -17,19 +22,33 @@ linkedList.prototype = {
    * add new node to the list
    */
   push: function(node) {
-  
+
+    if (this.length) {
+      this.insert(node);
+    }  
+
+    this.first = this.last = node;
+
+    this.length = 1;
   },
   /**
    * Insert node after existing end node
    */
   insert: function(node) {
-    
+
+    node.prev = this.last;
+    this.last.next = node;
+    this.last = node;
+    this.length++;
+
   }
 } 
 
 function node(data) {
+
   this.data = data;
   this.prev = this.next = null;
+
 }
 
 node.prototype = {
